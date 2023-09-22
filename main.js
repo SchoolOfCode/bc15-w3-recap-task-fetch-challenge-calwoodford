3ab2bcb43157e8e1f84043cc5e7ac9045bc55c3e
Cal Woodford <callum@woodford.info>
2023-09-22 21:39:16 +0100

    added: image change functionality, timeout

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let buttonText = document.getElementById("apodButton")
+
+function changeButtonText() {
+    buttonText.textContent = ("Get new APOD")}
+
+function stillThere() {
+    alert('Are you still there?');
+}
+setTimeout(stillThere, 100000);
+
 // Create button event
 let button = document.getElementById("apodButton")
 // Trigger event when button is clicked
@@ -8,16 +18,17 @@ button.addEventListener("click", changeText)
 
 async function returnAPOD() {
     console.log("Fetch request is running...");
+    buttonText.textContent = ("Please wait!")
 
     try {
-        const response = await fetch("https://api.nasa.gov/planetary/apod?count=100&api_key=jom8FVWWcBk1c7oAgbNiDRgtLNaOwGxR6KgF7kk2");
-        console.log(response);
+        const response = await fetch("https://api.nasa.gov/planetary/apod?count=20&api_key=jom8FVWWcBk1c7oAgbNiDRgtLNaOwGxR6KgF7kk2");
+
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log(data)
-        console.dir(data)
+        //Changes the button text
+
         return data
     } catch (error) {
         console.error('Error fetching APOD data:', error);
@@ -29,26 +40,19 @@ async function returnAPOD() {
 async function changeText(){
     // Take returnAPOD Function and store as variable
     const newText = await returnAPOD();
-    console.dir(newText)
 
-    // Looks for the 99th item in the object, this will change every time, this is important so that both the image and text are from the same object.
-    let random = 99
-
+    // Looks for the first item in the object, this will change every time, this is important so that both the image and text are from the same object.
+    let random = 1
 
     // Parse API data and return new, required variables.
     let newApodText = newText[random]["explanation"]
     let newApodImage = newText[random]["hdurl"]
 
-    let imageChanger
     let description = document.getElementById("apodText")
-    document.getElementById("apodImage").src = imageChanger
+    let imageElement = document.getElementById("apodImage")
 
+    imageElement.src = newApodImage;
     description.textContent = newApodText
-    imageChanger = newApodImage
-
-
-
-
-
-
+    changeButtonText()
 }
+
